9ff5ebca590c3dfc9139bb5fd2de59d75d44a99f
nbukhal <nikita.bukhal@noveogroup.com>
2013-12-18 20:50:48 +0700

    Add form

--- gcm-user-notification.py
+++ gcm-user-notification.py
@@ -1,15 +1,20 @@
 import cgi
-
+import json
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+api_key = "AIzaSyDiNlzQ9adtA1WojZjE_ZtUJ-u6XenRJ2w"
+
+registrationIds = "APA91bH91lUn-xpYLc4x098J3lH3BPqM54tLXdv0STLkJlvGPNvv1xga0wO_YZhkTNImvmrMuHZn5G8A8qaYnF-t-U14xupKado0SxvNt4d15Psyf8LJQvXzANNjRcK7sQ6JoxL2TxTUNJWxNU9GYzQf8BdjSqq_WeO09UfkufkdJNds6GuURys"
+
 
 class MainPage(webapp.RequestHandler):
     def get(self):
         self.response.out.write("""
           <html>
             <body>
-              <form action="https://android.googleapis.com/gcm/send" method="post">
+              <form action="/send" method="post">
                 <div><textarea name="content" rows="3" cols="60"></textarea></div>
                 <div><input type="submit" value="Sign Guestbook"></div>
               </form>
@@ -19,13 +24,15 @@ class MainPage(webapp.RequestHandler):
 
 class Guestbook(webapp.RequestHandler):
     def post(self):
+        content = cgi.escape(self.request.get('content'));
+        
         self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write(content)
         self.response.out.write('</pre></body></html>')
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/sign', Guestbook)],
+                                      ('/send', Guestbook)],
                                      debug=True)
 
 def main():
